50cad4c999bb700fb87ff750d910b59ad765f734
Calin Vlad <vlad.s.calin@gmail.com>
2017-05-28 10:06:53 +0300

    exposed MarkoviPy class in root module (#11) closes #10

--- markovipy/__init__.py
+++ markovipy/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from .markovipy import MarkoviPy
+
 __author__ = """Tasdik Rahman"""
 __email__ = 'prodicus@outlook.com'
 __version__ = '0.2.0'
+
+__all__ = [
+    'MarkoviPy'
+]
